f537c49dc9938ca7f00a1458731ae59952644e64
joseduquep <duquepiedrahitaalejandro@gmail.com>
2025-04-03 20:04:40 -0500

    Antes de crear mas protos funcionando maso

--- rabbitmq/producer.py
+++
@@ -1,6 +0,0 @@
-"""
-Este módulo define funciones para publicar mensajes en RabbitMQ.
-En este ejemplo, el Gateway importa la lógica "publish_to_rabbitmq"
-directamente, pero podrías usarlo aquí si deseas un flujo más modular.
-Actualmente, la lógica final está en gateway.py para simplificar.
-"""
